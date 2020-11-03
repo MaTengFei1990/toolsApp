4551459afccecdc0d3b6c384e060b391f065ca22
matf <1124061609@qq.com>
2020-11-03 13:57:42 +0800

    init commit

---
+++ androidToolsLib/src/main/java/com/holllysmart/androidtoolslib/AddBean.java
@@ -0,0 +1,27 @@
+package com.holllysmart.androidtoolslib;
+
+public class AddBean {
+
+    int a;
+    int b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int add() {
+        return a + b;
+    }
+}
